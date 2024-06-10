33b152a36858c096f37250257e4e96a9bfb13980
Роман Чижевский <r.chizhevsky@yandex.ru>
2024-06-10 21:36:15 +0300

    init

---
+++ src/main/java/ru/museum_hack_2024/MuseumHack2024Application.java
@@ -0,0 +1,13 @@
+package ru.museum_hack_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumHack2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseumHack2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/museum_hack_2024/MuseumHack2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.museum_hack_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumHack2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
